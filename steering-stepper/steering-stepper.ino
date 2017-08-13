4e998287bbceeb884e5f0fd65e83cfb275e6a42c
Eddie1337 <e.verheul91@gmail.com>
2017-08-13 20:31:23 +0200

    First test on bolderkar

--- steering-stepper/steering-stepper.ino
+++ steering-stepper/steering-stepper.ino
@@ -23,17 +23,20 @@ const int stepsPerRevolution = 200;  // change this to fit the number of steps p
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 
-void setup() {
+void setup() 
+  {
   // set the speed at 60 rpm:
   myStepper.setSpeed(15);
     pinMode(12, INPUT_PULLUP);
   // initialize the serial port:
   Serial.begin(9600);
-}
+  }
 
-void loop() {
+void loop() 
+{
   int sensorVal = digitalRead(12);
-  if (sensorVal == HIGH) {
+  if (sensorVal == HIGH) 
+  {
     for (int i = 0;i<=2;i++)
       {
         // step one revolution  in one direction:
@@ -42,19 +45,12 @@ void loop() {
         myStepper.step(stepsPerRevolution);
       }
     for (int i = 0;i<=2;i++)
-  {
+    {
       // step one revolution  in one direction:
-  Serial.println("counterclockwise");
-  Serial.println(i);
-  myStepper.step(-stepsPerRevolution);
-  }
+      Serial.println("counterclockwise");
+      Serial.println(i);
+      myStepper.step(-stepsPerRevolution);
      }
-
- // delay(100000);
-
-  // step one revolution in the other direction:
-//  Serial.println("counterclockwise");
-//  myStepper.step(-stepsPerRevolution);
-//  delay(1000);
+  }
 }
 
